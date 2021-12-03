548a3eee7f8121b1935c68ff0d2afe26c054c0a3
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-03 13:27:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import requests
 @app.route("/telegram", methods=["POST"])
 def telegram():
     token = "2140351582:AAEMn6lRZTeV3ekUICnWV82MD85gW3bx7mg"
-    dados = request.jsonmensagem = {"chat_id": dados["message"]["chat"]["id"], "text": "oi!"}
+    dados = request.jsonmensagem = {"chat_id": dados["message"]["chat"]["id"], "text": "Oi!"}
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     requests.post(url, data=mensagem)
     return "ok"
