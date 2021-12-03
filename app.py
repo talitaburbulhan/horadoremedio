c3fa7cdba288813efd3ab8d74c8a5ef20bc58823
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-03 10:31:18 -0300

    Coloca código da aula para ver se site funciona

--- app.py
+++ app.py
@@ -5,3 +5,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Olá, <b>pessoal</b>!</p>"
+
+### Site para o robô do telegram enviar dados das atualizações:
+
+
+from flask import request
+import requests
+
+@app.route("/telegram", methods=["POST"])
+def telegram():
+    token = "2140351582:AAEMn6lRZTeV3ekUICnWV82MD85gW3bx7mg"
+    dados = request.jsonmensagem = {"chat_id": dados["message"]["chat"]["id"], "text": "oi!"}
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    requests.post(url, data=mensagem)
+    return "ok"
+    
