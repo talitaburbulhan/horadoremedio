16a0ba35d257bdbada1780de24a1b7d48e406760
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-03 19:11:11 -0300

    Cria programa que será rodado periodicamente

---
+++ envia_mensagem_automatica.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+
+ token = os.environ["TELEGRAM_TOKEN"] 
+ url = f"https://api.telegram.org/bot{token}/sendMessage"
+ chat_id = 1007648629
+ text = "Olá, hora de tomar o remédio!"
+ mensagem = {"chat_id": chat_id, "text": text}
+ requests.post(url, data = message)
