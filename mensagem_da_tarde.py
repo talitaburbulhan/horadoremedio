a8debabfca5d29b81554d2d59faa9e2434a83de2
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-04 23:15:59 -0300

    Edita texto da mensagem

--- mensagem_da_tarde.py
+++ mensagem_da_tarde.py
@@ -5,6 +5,6 @@ import os
 token = os.environ["TELEGRAM_TOKEN"] 
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 chat_id = 1007648629, 1884457800
-text = "Oi Flávio, boa tarde! Agora é hora de tomar 2 florinefes/n 1 velus/n/n 1 azatioprina."
+text = "Oi Flávio, boa tarde! Agora é hora de tomar 2 florinefes, 1 velus, 1 azatioprina."
 mensagem = {"chat_id": chat_id, "text": text}
 requests.post(url, data = mensagem)
