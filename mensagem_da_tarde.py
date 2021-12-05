e2f757cfbac4f5ef04266e598c9c920a0c5afa73
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-04 23:09:57 -0300

    Cria mensagem para enviar no período da tarde

---
+++ mensagem_da_tarde.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+
+token = os.environ["TELEGRAM_TOKEN"] 
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+chat_id = 1007648629, 1884457800
+text = "Oi Flávio, boa tarde! Agora é hora de tomar 2 florinefes/n 1 velus/n/n 1 azatioprina."
+mensagem = {"chat_id": chat_id, "text": text}
+requests.post(url, data = mensagem)
