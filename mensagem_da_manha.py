dab772a9b740a2b11fc514a75c426a6789f728dc
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-04 23:37:24 -0300

    Edita chat_id

--- mensagem_da_manha.py
+++ mensagem_da_manha.py
@@ -4,7 +4,7 @@ import os
 
 token = os.environ["TELEGRAM_TOKEN"] 
 url = f"https://api.telegram.org/bot{token}/sendMessage"
-chat_id = [1007648629, 1884457800]
+chat_id = 1884457800
 text = "Olá, bom dia! É hora de tomar 2 florinefes, 1 mesalazina, 1 azatioprina."
 mensagem = {"chat_id": chat_id, "text": text}
 requests.post(url, data = mensagem)
