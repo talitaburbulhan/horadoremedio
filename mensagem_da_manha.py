b3829d7a4f26666e9da1362aa42b9b50e22b6796
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-04 23:04:56 -0300

    Renomeia arquivo e edita mensagem

--- mensagem_da_manha.py
+++ mensagem_da_manha.py
@@ -4,7 +4,7 @@ import os
 
 token = os.environ["TELEGRAM_TOKEN"] 
 url = f"https://api.telegram.org/bot{token}/sendMessage"
-chat_id = 1007648629
-text = "Olá, hora de tomar o remédio!"
+chat_id = 1007648629, 1884457800
+text = "Olá, bom dia! É hora de tomar 2 florinefes/n 1 mesalazina/n 1 azatioprina."
 mensagem = {"chat_id": chat_id, "text": text}
 requests.post(url, data = mensagem)
