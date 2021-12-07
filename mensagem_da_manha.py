71ca8d653f163a0200a16d23622d766fa7181ebc
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-07 19:06:16 -0300

    Configura mensagem para linguagem html.

--- mensagem_da_manha.py
+++ mensagem_da_manha.py
@@ -6,6 +6,6 @@ token = os.environ["TELEGRAM_TOKEN"]
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 chat_ids = 1007648629, 1884457800
 for chat_id in chat_ids:
-  text = "Olá, bom dia! É hora de tomar 2 florinefes, 1 mesalazina, 1 azatioprina."
-  mensagem = {"chat_id": chat_id, "text": text}
+  text = "<b>Olá, bom dia!</b>\nÉ hora de tomar: \n2️⃣florinefes, \n1️⃣ mesalazina, \n1️⃣ azatioprina."
+  mensagem = {"chat_id": chat_id, "text": text, "parse_mode": HTML}
   requests.post(url, data = mensagem)
