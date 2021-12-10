39219d6972e4d2632f052a135b8c47967d52a6f3
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-10 20:25:13 -0300

    Update mensagem_da_noite.py

--- mensagem_da_noite.py
+++ mensagem_da_noite.py
@@ -6,6 +6,6 @@ token = os.environ["TELEGRAM_TOKEN"]
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 chat_ids = 1007648629, 1884457800
 for for_id in chat_ids:
-  text = "Olha eu aqui de novo!! Antes de dormir não esqueça de tomar 1 azatioprina e 1 luvox."
-  mensagem = {"chat_id": chat_id, "text": text}
+  text = "Olha eu aqui de novo 🌚! Antes de dormir <b>não esqueça</b> de tomar 1️⃣ azatioprina e 1️⃣ luvox."
+  mensagem = {"chat_id": chat_id, "text": text, "parse_mode": HTML}
   requests.post(url, data = mensagem)
