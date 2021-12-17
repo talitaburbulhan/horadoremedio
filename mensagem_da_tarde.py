a03a349c95b65965dfc2607488a10a408ea77b66
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-17 15:19:57 -0300

    Update mensagem_da_tarde.py

--- mensagem_da_tarde.py
+++ mensagem_da_tarde.py
@@ -7,5 +7,5 @@ url = f"https://api.telegram.org/bot{token}/sendMessage"
 chat_ids = 1007648629, 1884457800
 for chat_id in chat_ids:
   text = "<b>Oi Flávio, boa tarde!</b> \nAgora é hora de tomar 2 florinefes, 1 velus, 1 azatioprina."
-  mensagem = {"chat_id": chat_id, "text": text, "parse_mode": HTML}
+  mensagem = {"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
   requests.post(url, data = mensagem)
