9a4f5e7d4d2200d796c85ba06bb563384d1b2b60
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-17 20:47:45 -0300

    corrige erro de sintaxe

--- mensagem_da_noite.py
+++ mensagem_da_noite.py
@@ -5,7 +5,7 @@ import os
 token = os.environ["TELEGRAM_TOKEN"] 
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 chat_ids = 1007648629, 1884457800
-for for_id in chat_ids:
+for chat_id in chat_ids:
   text = "Olha eu aqui de novo 🌚! Antes de dormir <b>não esqueça</b> de tomar 1️⃣ azatioprina e 1️⃣ luvox."
   mensagem = {"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
   requests.post(url, data = mensagem)
