323756a4ae5cad18157b6afc4ece1b6b3a629eff
Talita Burbulhan <89229665+talitaburbulhan@users.noreply.github.com>
2021-12-19 20:42:50 -0300

    Update mensagem_da_manha.py

--- mensagem_da_manha.py
+++ mensagem_da_manha.py
@@ -6,6 +6,6 @@ token = os.environ["TELEGRAM_TOKEN"]
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 chat_ids = 1007648629, 1884457800
 for chat_id in chat_ids:
-  text = "<b>Olá, bom dia!</b>🌞\n\nÉ hora de tomar: \n\n2️⃣Florinefes\n1️⃣ Mesalazina\n1️⃣ Azatioprina."
+  text = "<b>Olá, bom dia!</b> 🌞\n\nÉ hora de tomar: \n\n2️⃣ Florinefes\n\n1️⃣ Mesalazina\n\n1️⃣ Azatioprina"
   mensagem = {"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
   requests.post(url, data = mensagem)
